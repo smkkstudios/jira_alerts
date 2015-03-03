ebb834af043cf297bd231e0a2965062341ddd403
Josh <elementcontrol@gmail.com>
2015-03-02 20:32:32 -0500

    bug fixn

--- api/controllers/JiraController.js
+++ api/controllers/JiraController.js
@@ -15,11 +15,11 @@ module.exports = {
   update: function (req, res) {
     var socket = req.socket;
     var io = sails.io;
-    var test = JiraAlert.create(req.body).exec(function(err,post){
+    var Test = test.create({comment:'this is a working model'}}).exec(function(err,post){
       if(err){
         console.log(err);
       }
-      console.log('model created beyo: ',test);
+      console.log('model created beyo: ');
     })
 	  io.sockets.emit('jiraAlert', {thisIs: req.body});
     return res.send('it has been done')
