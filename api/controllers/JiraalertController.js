5647a1c8936f34a94c0c696e78527a23a1ec7ec7
Josh <elementcontrol@gmail.com>
2015-03-02 23:55:15 -0500

    jira alert controller

---
+++ api/controllers/JiraalertController.js
@@ -0,0 +1,11 @@
+/**
+ * JiraalertController
+ *
+ * @description :: Server-side logic for managing jiraalerts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
