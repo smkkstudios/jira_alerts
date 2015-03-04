5b5c5a762fc1a4a098d8f19c1921857b35cea2e9
Josh <elementcontrol@gmail.com>
2015-03-03 22:41:30 -0500

    updated code

--- assets/js/controllers/jiraalert_controllers.js
+++ assets/js/controllers/jiraalert_controllers.js
@@ -6,7 +6,7 @@
 			updateModel:function(){
 				var alert = this.get('model');
 					alert.set('user', {displayName:'meho dodo'});
-					alert.save();
+					this.get('model').save();
 			}
 		}
 	});
